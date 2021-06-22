438eda33a65da313fc0fa187b8399914cd900513
Gustavo Meyer <gustmeyersax@gmail.com>
2021-06-22 10:32:12 -0300

    First implementation of findById

--- backend/src/main/java/com/gustmeyer/desafio01/resources/ClientResource.java
+++ backend/src/main/java/com/gustmeyer/desafio01/resources/ClientResource.java
@@ -1,10 +1,26 @@
 package com.gustmeyer.desafio01.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gustmeyer.desafio01.entity.Client;
+import com.gustmeyer.desafio01.services.ClientService;
+
 @RestController
-@RequestMapping(value = "/client")
+@RequestMapping(value = "/clients")
 public class ClientResource {
+	
+	@Autowired
+	private ClientService service;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Client> findById(@PathVariable Long id){
+		Client client = service.findById(id);
+		return ResponseEntity.ok().body(client);
+	}
 
 }
--- backend/src/main/java/com/gustmeyer/desafio01/services/ClientService.java
+++ backend/src/main/java/com/gustmeyer/desafio01/services/ClientService.java
@@ -1,8 +1,11 @@
 package com.gustmeyer.desafio01.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.gustmeyer.desafio01.entity.Client;
 import com.gustmeyer.desafio01.repositories.ClientRepository;
 
 @Service
@@ -11,6 +14,9 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 	
-	
+	public Client findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		return obj.get();
+	}
 
 }
