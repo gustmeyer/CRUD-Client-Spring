a92ad84f13ef80ef21626220ea0d70279cf3c4b9
Gustavo Meyer <gustmeyersax@gmail.com>
2021-06-22 12:07:19 -0300

    start to implement findAllPaged (not working yet)

--- backend/src/main/java/com/gustmeyer/desafio01/resources/ClientResource.java
+++ backend/src/main/java/com/gustmeyer/desafio01/resources/ClientResource.java
@@ -1,10 +1,17 @@
 package com.gustmeyer.desafio01.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gustmeyer.desafio01.entity.Client;
@@ -21,6 +28,29 @@ public class ClientResource {
 	public ResponseEntity<Client> findById(@PathVariable Long id){
 		Client client = service.findById(id);
 		return ResponseEntity.ok().body(client);
+	}
+	
+	@GetMapping(value = "/all")
+	public ResponseEntity<List<Client>> findAll(){
+		List<Client> list = new ArrayList<>();
+		list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping
+	public ResponseEntity<Page<Client>> findAll(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "8") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "NAME") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction
+			){
+			
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		Page<Client> list = service.findAllPaged(pageRequest);
+		
+		return ResponseEntity.ok().body(list);
+		
 	}
 
 }
--- backend/src/main/java/com/gustmeyer/desafio01/services/ClientService.java
+++ backend/src/main/java/com/gustmeyer/desafio01/services/ClientService.java
@@ -1,8 +1,11 @@
 package com.gustmeyer.desafio01.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import com.gustmeyer.desafio01.entity.Client;
@@ -17,6 +20,16 @@ public class ClientService {
 	public Client findById(Long id) {
 		Optional<Client> obj = repository.findById(id);
 		return obj.get();
+	}
+	
+	public List<Client> findAll() {
+		List<Client> obj = repository.findAll();
+		return obj;
+	}
+	
+	public Page<Client> findAllPaged(PageRequest pageRequest) {
+		Page<Client> obj = repository.findAll(pageRequest);
+		return obj;
 	}
 
 }
