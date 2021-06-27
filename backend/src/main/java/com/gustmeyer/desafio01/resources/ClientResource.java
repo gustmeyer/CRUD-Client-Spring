4c102c2ac269a0c1d11df7048b55f108d01a8cc0
Gustavo Meyer <gustmeyersax@gmail.com>
2021-06-27 09:10:17 -0300

    DTO implementation done

--- backend/src/main/java/com/gustmeyer/desafio01/resources/ClientResource.java
+++ backend/src/main/java/com/gustmeyer/desafio01/resources/ClientResource.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.gustmeyer.desafio01.entity.Client;
+import com.gustmeyer.desafio01.dtos.ClientDTO;
 import com.gustmeyer.desafio01.services.ClientService;
 
 @RestController
@@ -28,14 +29,13 @@ public class ClientResource {
 	private ClientService service;
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Client> findById(@PathVariable Long id){
-		Client client = service.findById(id);
+	public ResponseEntity<ClientDTO> findById(@PathVariable Long id){
+		ClientDTO client = service.findById(id);
 		return ResponseEntity.ok().body(client);
 	}
 	
-	
 	@GetMapping
-	public ResponseEntity<Page<Client>> findAll(
+	public ResponseEntity<Page<ClientDTO>> findAll(
 			@RequestParam(value = "page", defaultValue = "0") Integer page,
 			@RequestParam(value = "linesPerPage", defaultValue = "8") Integer linesPerPage,
 			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
@@ -43,13 +43,12 @@ public class ClientResource {
 			){
 			
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
-		Page<Client> list = service.findAllPaged(pageRequest);
+		Page<ClientDTO> list = service.findAllPaged(pageRequest);
 		return ResponseEntity.ok().body(list);
-		
 	}
 	
 	@PostMapping
-	public ResponseEntity<Client> insert(@RequestBody Client client) {
+	public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO client) {
 		client = service.add(client);
 		
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(client.getId()).toUri();
@@ -57,9 +56,15 @@ public class ClientResource {
 	}
 	
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<Client> update(@PathVariable Long id, @RequestBody Client client ){
+	public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO client ){
 		client = service.update(id, client);
 		return ResponseEntity.ok().body(client);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<ClientDTO> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
--- backend/src/main/java/com/gustmeyer/desafio01/services/ClientService.java
+++ backend/src/main/java/com/gustmeyer/desafio01/services/ClientService.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.gustmeyer.desafio01.dtos.ClientDTO;
 import com.gustmeyer.desafio01.entity.Client;
 import com.gustmeyer.desafio01.repositories.ClientRepository;
 
@@ -17,37 +18,51 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 	
-	public Client findById(Long id) {
+	public ClientDTO findById(Long id) {
 		Optional<Client> obj = repository.findById(id);
-		return obj.get();
+		ClientDTO client = new ClientDTO(obj.get());
+		return client;
 	}
 	
 	@Transactional(readOnly = true)
-	public Page<Client> findAllPaged(PageRequest pageRequest) {
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
 		Page<Client> obj = repository.findAll(pageRequest);
-		return obj;
+		
+		return obj.map(x -> new ClientDTO(x));
 	}
 	
 	@Transactional
-	public Client add(Client client) {
-		repository.save(client);
-		
+	public ClientDTO add(ClientDTO client) {
+		Client c = new Client();
+		dtoToEntity(c, client);
+		repository.save(c);
 		return client;
 	}
 
 	@Transactional
-	public Client update(Long id, Client client) {
+	public ClientDTO update(Long id, ClientDTO client) {
 		Client c = repository.getOne(id);
 		
-		c.setBirthDate(client.getBirthDate());
-		c.setChildren(client.getChildren());
-		c.setCpf(client.getCpf());
-		c.setIncome(client.getIncome());
-		c.setName(client.getName());
+		dtoToEntity(c, client);
 		
 		c = repository.save(c);
-		
-		return c;
+		return client;
 	}
-
+	
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
+	
+	public Client dtoToEntity(Client client, ClientDTO dto) {
+		
+		client.setBirthDate(dto.getBirthDate());
+		client.setChildren(dto.getChildren());
+		client.setCpf(dto.getCpf());
+		client.setIncome(dto.getIncome());
+		client.setName(dto.getName());
+		
+		return client;
+		
+	}
+	
 }
